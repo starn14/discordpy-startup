7b29f4ece309bec492f565cd9a624ef3de44a455
starn14 <70058743+starn14@users.noreply.github.com>
2020-08-22 20:56:04 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 
 import discord
 
-TOKEN = 'NzQ2NjY0NjM1Nzc5OTA3NjUy.X0DnoQ.gQnoDV3lXEEcEDRuSsLohfAz4BY'
+TOKEN = 'NzQ2NjY0NjM1Nzc5OTA3NjUy.X0DnoQ.1oNXxQZT77Cy5uo6hZldOw0b-Is'
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
