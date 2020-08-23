a9dc59bbfcecb2d31ecd4f936061b998ddde25bd
starn14 <70058743+starn14@users.noreply.github.com>
2020-08-23 17:55:46 +0900

    Update test.py

--- test.py
+++ test.py
@@ -20,9 +20,8 @@ async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
+    if ':raised_hand:' in message.content:
+        await message.channel.send('hogehuga')
 
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
