4469f19372db4cc8392c2b3897ab8ddfa1d99841
starn14 <70058743+starn14@users.noreply.github.com>
2020-08-25 00:19:59 +0900

    Update project_winter_bot.py

--- project_winter_bot.py
+++ project_winter_bot.py
@@ -7,7 +7,6 @@ import random
 import discord
 
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
-os.environ['PW_BOT_MSG_ID'] = '0'
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
@@ -37,13 +36,13 @@ async def on_message(message):
         match2 = re.match(r'^/募集(\D*)(?P<start_hour>[0-9]{1,2})-(?P<end_hour>[0-9]{1,2})(\D*)$', message.content)
         if match1:
             start_hour = match1.group('start_hour')
-            await recruitment_participants(message.channel, start_hour=start_hour)
+            await recruitment_participants(message, start_hour=start_hour)
         elif match2:
             start_hour = match2.group('start_hour')
             end_hour = match2.group('end_hour')
-            await recruitment_participants(message.channel, start_hour=start_hour, end_hour=end_hour)
+            await recruitment_participants(message, start_hour=start_hour, end_hour=end_hour)
         else:
-            await recruitment_participants(message.channel)
+            await recruitment_participants(message)
         return
     if client.user in message.mentions:
         await reply(message.channel, message.author)
@@ -79,15 +78,11 @@ async def reply(channel, author):
 @client.event
 async def on_raw_reaction_add(payload):
     """メッセージにリアクションがつくと呼ばれる"""
-
-    if int(os.environ['PW_BOT_MSG_ID']) != payload.message_id:
-        # 最新の募集メッセージについたリアクションでなければ返す
-        return
         
     # channel_id から Channel オブジェクトを取得
     channel = client.get_channel(payload.channel_id)
-    # 最新の募集メッセージのmessageオブジェクトを取得
-    message = await channel.fetch_message(int(os.environ['PW_BOT_MSG_ID']))
+    # リアクションのついた募集メッセージのmessageオブジェクトを取得
+    message = await channel.fetch_message(payload.message_id)
 
     user_list = []
     # メッセージについているリアクションを取得
@@ -117,12 +112,8 @@ async def on_raw_reaction_add(payload):
 async def list_participants(channel):
     """現在の参加者の一覧を表示"""
 
-    # 最新の募集メッセージのmessageオブジェクトを取得
-    try:
-        message = await channel.fetch_message(int(os.environ['PW_BOT_MSG_ID']))
-    except discord.errors.NotFound:
-        await channel.send('募集メッセージが見つかりません')
-        return
+    # 過去のメッセージを遡って募集メッセージを探す
+    message = await get_last_recruitment_message(channel)
 
     user_list = []
     # メッセージについているリアクションを取得
@@ -147,12 +138,8 @@ async def list_participants(channel):
 async def appear_number(channel):
     """現在の参加人数を表示する"""
 
-    # 最新の募集メッセージのmessageオブジェクトを取得
-    try:
-        message = await channel.fetch_message(int(os.environ['PW_BOT_MSG_ID']))
-    except discord.errors.NotFound:
-        await channel.send('募集メッセージが見つかりません')
-        return
+    # 過去のメッセージを遡って募集メッセージを探す
+    message = await get_last_recruitment_message(channel)
 
     user_list = []
     # メッセージについているリアクションを取得
@@ -170,18 +157,21 @@ async def appear_number(channel):
     max_persons = 8 - len(user_list)
     await channel.send('現在の参加者は%s人です\n@%s-%s' % (str(len(user_list)), min_persons, max_persons))
 
-async def recruitment_participants(channel, start_hour=None, end_hour=None):
+async def recruitment_participants(message, start_hour=None, end_hour=None):
     """募集をかける"""
     if (not start_hour) and (not end_hour):
-        await channel.send('@here\nProjectWinter対戦募集します\n6人～8人集まれば開催します\n参加する方はこのメッセージにリアクションをつけてください')
+        await message.channel.send('@here\nProjectWinter対戦募集します\n6人～8人集まれば開催します\n参加する方はこのメッセージにリアクションをつけてください')
     elif start_hour and (not end_hour):
-        await channel.send('@here\nProjectWinter対戦募集します\n%s時頃に6人～8人集まれば開催します\n参加する方はこのメッセージにリアクションをつけてください' % start_hour)
+        await message.channel.send('@here\nProjectWinter対戦募集します\n%s時頃に6人～8人集まれば開催します\n参加する方はこのメッセージにリアクションをつけてください' % start_hour)
     else:
-        await channel.send('@here\nProjectWinter対戦募集します\n%s時～%s時頃に6人～8人集まれば開催します\n参加する方はこのメッセージにリアクションをつけてください' % (start_hour, end_hour))
+        await message.channel.send('@here\nProjectWinter対戦募集します\n%s時～%s時頃に6人～8人集まれば開催します\n参加する方はこのメッセージにリアクションをつけてください' % (start_hour, end_hour))
 
-    # 募集メッセージのIDを記録
-    # このメッセージについたリアクションで参加者を判断する
-    os.environ['PW_BOT_MSG_ID'] = str(channel.last_message_id)
+async def get_last_recruitment_message(channel):
+    async for message in channel.history(limit=500):
+        if not message.author.bot:
+            continue
+        if message.content.startswith('@here\nProjectWinter対戦募集します'):
+            return message
 
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
